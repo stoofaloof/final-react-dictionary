a794e2168838af63dcb33c55639cbe26777f2382
Steph C <stephanie.wy.chow@gmail.com>
2021-08-31 16:34:13 -0400

    Dictionary API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,19 @@
+import axios from "axios";
 import React, { useState } from "react";
 
 export default function Dictionary() {
   const [searchWord, setsearchWord] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${searchWord}`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${searchWord}`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleSubmit(event) {
