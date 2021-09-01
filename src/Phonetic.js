c60a41f23d178b7cd2589a00bdb9834a4219d809
Steph C <stephanie.wy.chow@gmail.com>
2021-08-31 21:04:42 -0400

    Add phonetics

---
+++ src/Phonetic.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Phonetic(props) {
+  return (
+    <div className="Phonetic">
+      {props.phonetic.text} ||
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+        {""} Listen
+      </a>
+    </div>
+  );
+}
